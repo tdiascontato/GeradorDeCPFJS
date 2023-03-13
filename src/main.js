c14f22d955fbb26156c200cfb435a1ea850b46f7
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-03-13 13:22:00 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import GeraCPF from './assets/modules/GeraCPF';
+import './assets/css/stylus.css';
+/*(function() {
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    console.log(gera.geraNovoCpf());
+})();*/
